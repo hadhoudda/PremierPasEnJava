6fa87ae78af804309d27f3ab4f90c5e8936ebd3c
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-11-26 11:30:25 +0100

    first commit

---
+++ src/org/paumard/hello/model/User.java
@@ -0,0 +1,45 @@
+package org.paumard.hello.model;
+
+import java.util.Objects;
+
+public class User {
+	private String name;
+	private int age;
+	
+	
+	
+	public User() {
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", age=" + age + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(age, name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return age == other.age && Objects.equals(name, other.name);
+	}
+	
+}
