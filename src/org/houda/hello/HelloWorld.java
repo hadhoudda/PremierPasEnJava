68906d9f261295b0721c808c5f4547f61b19fc79
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-11-28 22:09:21 +0100

    cmmit 3

--- src/org/houda/hello/HelloWorld.java
+++ src/org/houda/hello/HelloWorld.java
@@ -3,12 +3,20 @@ package org.houda.hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		String text = "Bonjour tout le monde";
-		text = text.toUpperCase();	
-		int length = text.length();
-		System.out.println(text);
-		System.out.println("le nombre de caracters dans '" + text + " ' est:"+length);
+		sayText("Bonjour tout le monde");
 
+	}
+	
+	private static void sayText(String text) {
+		text = text.toUpperCase();	
+		/*
+		int length = text.length();
+		 * System.out.println(text);
+		System.out.println("le nombre de caracters dans '" + text + " ' est:"+length);
+		*/
+		System.out.println(text);
+		System.out.println("l'heure est :");
+		System.out.println(java.time.LocalTime.now());
 	}
 
 }
