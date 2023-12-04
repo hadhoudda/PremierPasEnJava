47e792b1f169db8c283ffebee98221a2d84f9700
Houda Ben Abdallah <ahhouda@yahoo.fr>
2023-12-04 21:14:51 +0100

    commit 4

--- src/org/houda/hello/HelloWorld.java
+++ src/org/houda/hello/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		sayText("Bonjour tout le monde");
+		Person p1= new Person("Abdel", 48, "masculin");
+		Familly f1= new Familly("Ben Aoun",7,p1);
+		System.out.println(f1.nomberPerson);
 
 	}
 	
@@ -17,6 +20,27 @@ public class HelloWorld {
 		System.out.println(text);
 		System.out.println("l'heure est :");
 		System.out.println(java.time.LocalTime.now());
+	}
+	// declaration des class
+	static class Familly {
+		String prenomFammilly;
+		int nomberPerson;
+		Person person;
+		public Familly(String prenomFammilly,int nomberPerson,Person person ) {
+			this.prenomFammilly = prenomFammilly;
+			this.nomberPerson = nomberPerson;
+			this.person = person;
+		}
+	}
+	static class Person{
+		String name;
+		int age;
+		String gener;
+		public Person(String name,int age,String gener){
+			this.name = name;
+			this.age = age;
+			this.gener = gener;
+		}
 	}
 
 }
